eb00b9ccc0817cb2553fb6acfffede04dbcc6e80
rgiri1706 <rgiri1706@gmail.com>
2018-07-11 17:42:27 +0530

    Signup Ember

--- app/routes/admin/login.js
+++ app/routes/admin/login.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+   model(){
+     return this.store.createRecord('login');
+  },
+   actions: {
+
+    save(newLibrary) {
+      newLibrary.save();
+      alert('SignUp successfull');
+        newLibrary.save().then(() => this.transitionTo('/'));
+    },
+
+    willTransition() {
+      // rollbackAttributes() removes the record from the store
+      // if the model 'isNew'
+      this.controller.get('model').rollbackAttributes();
+    }
+  }
 });
